db3533271df1b31544f8a2a66186a4a84c6ef629
santosh-chaugule <santoshchaugule1697@gmail.com>
2023-05-17 12:18:02 +0530

    !st

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+//import area
+
 import logo from './logo.svg';
+import logo2 from './images.png'
 import './App.css';
 
+//1:function defination
 function App() {
+
+  //every function return something
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo santosh" alt="logo2" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Name is Santosh And I am MERN Stack Developer
         </p>
         <a
-          className="App-link"
+          className="App-logo"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -22,4 +30,9 @@ function App() {
   );
 }
 
+
+//Export area
 export default App;
+//function sum(x,y){
+ // return
+//}
